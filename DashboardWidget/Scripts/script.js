8e656adbbc3b63c669487932ac09be395a27f047
Jon Coello <joncoello@gmail.com>
2017-07-10 20:29:38 +0100

    load caarousel from request

--- DashboardWidget/Scripts/script.js
+++ DashboardWidget/Scripts/script.js
@@ -1,10 +1,21 @@
 (function () {
+    var pictureUrl = 'http://localhost:28867/api/pictures';
     var widget = {
         id: 0,
         name: 'Dashboard Widget',
         setupWidget: function (element) {
-            $(element).carousel("pause").removeData();
-            $(element).carousel(0);
+            var startIndex = 0;
+            $.get(pictureUrl, function (data) {
+                data.forEach(function (item) {
+                    var template = '<div class=\'item\'><img src=\'' + pictureUrl + '/' + item.id + '\'><div class=\'carousel-caption\'><h4>' + item.name + '</h4></div></div>';
+                    $('.carousel-indicators').append('<li data-target="#' + element.id + '" data-slide-to="' + startIndex + '"></li>');
+                    $('.carousel-inner').append(template);
+                    startIndex++;
+                });
+                $('.item', element).first().addClass('active');
+                $(element).carousel();
+                console.log('carousel initiated');
+            });
         },
         removeWidget: function (element) {
         },
--- DashboardWidget/Scripts/script.ts
+++ DashboardWidget/Scripts/script.ts
@@ -1,14 +1,36 @@
 ﻿(function () {
 
+    var pictureUrl = 'http://localhost:28867/api/pictures';
+
     let widget: WidgetComponent =
         {
             id: 0,
             name: 'Dashboard Widget',
             setupWidget: (element: Element) => {
 
-                $(element).carousel("pause").removeData();
-                $(element).carousel(0);
+                var startIndex = 0;
+                
+                $.get(pictureUrl, function (data) {
+                    
+                    (<Array<any>>data).forEach((item) => {
+                        
+                        var template = '<div class=\'item\'><img src=\''+ pictureUrl +'/'+ item.id +'\'><div class=\'carousel-caption\'><h4>' + item.name + '</h4></div></div>';
 
+                        $('.carousel-indicators').append('<li data-target="#' + element.id + '" data-slide-to="' + startIndex + '"></li>');
+                        $('.carousel-inner').append(template)
+
+                        startIndex++;
+                        
+                    });
+                    
+                    $('.item', element).first().addClass('active');
+
+                    $(element).carousel();
+
+                    console.log('carousel initiated');
+                    
+                });
+                
             },
             removeWidget: (element: Element) => {
 
